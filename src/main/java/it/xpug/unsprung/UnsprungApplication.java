e2788e15642daf4c0c2a386ed81d88e37e18488a
Matteo Vaccari <vaccari@pobox.com>
2017-10-28 13:30:03 +0200

    initial import

---
+++ src/main/java/it/xpug/unsprung/UnsprungApplication.java
@@ -0,0 +1,12 @@
+package it.xpug.unsprung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsprungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsprungApplication.class, args);
+	}
+}
