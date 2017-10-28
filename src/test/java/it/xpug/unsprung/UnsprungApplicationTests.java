b2dee95df332442e93d16909c5bed15bed92ffdd
Matteo Vaccari <vaccari@pobox.com>
2017-10-28 13:46:49 +0200

    context test works

--- src/test/java/it/xpug/unsprung/UnsprungApplicationTests.java
+++ src/test/java/it/xpug/unsprung/UnsprungApplicationTests.java
@@ -3,10 +3,12 @@ package it.xpug.unsprung;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 public class UnsprungApplicationTests {
 
 	@Test
