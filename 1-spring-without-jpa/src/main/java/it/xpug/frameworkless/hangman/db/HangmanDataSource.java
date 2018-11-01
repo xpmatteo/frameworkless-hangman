14dbaf875e1afa87a7dd79b691dff0bc59b71808
Matteo Vaccari <mvaccari@thoughtworks.com>
2018-11-01 23:56:45 +0100

    Add non-jpa datasource

--- 1-spring-without-jpa/src/main/java/it/xpug/frameworkless/hangman/db/HangmanDataSource.java
+++ 1-spring-without-jpa/src/main/java/it/xpug/frameworkless/hangman/db/HangmanDataSource.java
@@ -2,13 +2,20 @@ package it.xpug.frameworkless.hangman.db;
 
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HangmanDataSource extends MysqlDataSource {
-    public HangmanDataSource() {
-        setDatabaseName("hangman");
-        setUser("hangman");
-        setPassword("hangman");
+
+    // The actual values are set in the application.properties file
+    public HangmanDataSource(
+            @Value("${datasource.dbname}")   String dbname,
+            @Value("${datasource.username}") String username,
+            @Value("${datasource.password}") String password)
+    {
+        setDatabaseName(dbname);
+        setUser(username);
+        setPassword(password);
     }
 }
