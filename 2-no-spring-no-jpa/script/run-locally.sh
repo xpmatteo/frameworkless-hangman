c8034f815dea7661b56ae59278ec5fe382ace7eb
Matteo Vaccari <mvaccari@thoughtworks.com>
2018-11-03 17:25:02 +0100

    2: can run a hello world servlet

--- 2-no-spring-no-jpa/script/run-locally.sh
+++ 2-no-spring-no-jpa/script/run-locally.sh
@@ -3,8 +3,8 @@
 set -e
 cd $(dirname $0)/..
 
-./gradlew build -x test
+./gradlew shadowJar -x test
 
 exec java \
   -XX:TieredStopAtLevel=1 -noverify \
-  -jar "build/libs/hangman-0.0.1-SNAPSHOT.jar" $*
+  -jar "build/libs/hangman-0.0.1-SNAPSHOT-all.jar" $*
--- 2-no-spring-no-jpa/src/main/java/it/xpug/frameworkless/hangman/Main.java
+++ 2-no-spring-no-jpa/src/main/java/it/xpug/frameworkless/hangman/Main.java
@@ -3,10 +3,10 @@ package it.xpug.frameworkless.hangman;
 
 import it.xpug.hangman.web.toolkit.ReusableJettyApp;
 
-public class HangmanApplication {
+public class Main {
 
 	public static void main(String[] args) {
 		ReusableJettyApp app = new ReusableJettyApp(HangmanServlet.class);
-		app.start(8080, "src/main/webapp");
+		app.start(8080);
 	}
 }
