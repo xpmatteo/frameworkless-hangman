d89835753c0046a08f83805f0921281b85093133
Matteo Vaccari <mvaccari@thoughtworks.com>
2018-11-04 21:42:39 +0100

    fix travis (?) -- 2

--- script/ci.sh
+++ script/ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-
-(cd 0-spring-jpa; ./gradlew test)
-(cd 1-spring-without-jpa; ./gradlew test)
-(cd 2-no-spring-no-jpa; ./gradlew test)
+(cd 0-spring-jpa;         echo "" | script/create-local-databases.sh)
+(cd 0-spring-jpa;         ./gradlew test --console plain)
+(cd 1-spring-without-jpa; ./gradlew test --console plain)
+(cd 2-no-spring-no-jpa;   ./gradlew test --console plain)
 
