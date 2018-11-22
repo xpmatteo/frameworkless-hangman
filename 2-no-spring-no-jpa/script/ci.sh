81609c8e4a56f3295a9e6c06c35705307ce61904
Matteo Vaccari <mvaccari@thoughtworks.com>
2018-11-22 16:45:48 +0100

    2: make it easier to check quickly I didn't break anything

--- 2-no-spring-no-jpa/script/ci.sh
+++ 2-no-spring-no-jpa/script/ci.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
-echo "" | ./script/create-local-databases.sh
-./gradlew clean build --console plain
+set -e
+cd "$(dirname "$0")/.."
+
+function server_is_live {
+  curl --silent -o /dev/null localhost:8080
+}
+
+function server_is_gone {
+  ! server_is_live
+}
+
+function wait_until {
+  n=0
+  max=40
+  until "$@"; do
+    if [[ $(( n++ )) = $max ]]; then
+      echo "TIMEOUT waiting for $@"
+      return 1
+    fi
+    echo -n .
+    sleep 1
+  done
+}
+
+if server_is_live; then
+  echo "Server is already running"
+  exit 1
+fi
+
+echo "" | script/create-local-databases.sh
+./gradlew clean test --console=plain
+script/run-locally.sh --logging.level.root=WARN --spring.main.banner_mode=off &
+pid=$!
+wait_until server_is_live
+script/acceptance-test.sh
+kill $pid
+wait_until server_is_gone
